963e684a1183d882c29d2f48201163476258e866
abuabdulloh9599 <david@digitalnutrition.info>
2025-07-02 16:59:20 +0900

    feat: add lightColors && darkColors

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const lightColors = {
+  background: '#ffffff',
+  text: '#000000',
+  tint: '#007AFF',
+  tabInactive: '#999999',
+};
+
+export const darkColors = {
+  background: '#000000',
+  text: '#ffffff',
+  tint: '#0A84FF',
+  tabInactive: '#888888',
+};
