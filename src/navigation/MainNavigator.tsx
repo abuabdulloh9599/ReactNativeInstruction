bdf14bbef2891d435aa4659a4537b0170717a491
abuabdulloh9599 <david@digitalnutrition.info>
2025-07-02 17:27:01 +0900

    feat: add useAuthStore

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
+import AuthNavigator from './AuthNavigator';
+import { useAuthStore } from '../store/authStore';
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="HomeTabs"
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      {isAuthenticated ? (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Tabs" component={TabNavigator} />
+        </Stack.Navigator>
+      ) : (
+        <AuthNavigator />
+      )}
     </NavigationContainer>
   );
 }
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import { Apple, Mail } from 'lucide-react-native';
+import { useAuthStore } from '../../store/authStore';
 
 export default function LoginScreen() {
+  const isIOS = Platform.OS === 'ios';
+  const login = useAuthStore(state => state.login);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>🔐 Login Screen</Text>
+
+      <View style={styles.socialContainer}>
+        {isIOS && (
+          <TouchableOpacity style={styles.iconButton} onPress={login}>
+            <Apple size={28} color="#000" />
+          </TouchableOpacity>
+        )}
+
+        <TouchableOpacity style={styles.iconButton} onPress={login}>
+          <Mail size={28} color="#000" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 20 },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  socialContainer: {
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: 20,
+  },
+  iconButton: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#eee',
+  },
 });
