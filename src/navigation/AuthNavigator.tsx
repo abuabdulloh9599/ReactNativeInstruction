34697aa6ad36acd5b677f9075a188a8edf9326af
abuabdulloh9599 <david@digitalnutrition.info>
2025-07-03 09:46:29 +0900

    feat: add hasRegistered and register to useAuthStore

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
 import RegisterScreen from '../screens/auth/RegisterScreen';
 import { StatusBar } from 'react-native';
+import { useAuthStore } from '../store/authStore';
 
 export type AuthStackParamList = {
   Login: undefined;
@@ -12,17 +13,23 @@ export type AuthStackParamList = {
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 export default function AuthNavigator() {
+  const hasRegistered = useAuthStore(state => state.hasRegistered);
+
   return (
     <>
       <StatusBar backgroundColor={'#000'} />
 
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName={hasRegistered ? 'Login' : 'Register'}>
         <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </>
   );
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -2,12 +2,16 @@ import { create } from 'zustand';
 
 interface AuthState {
   isAuthenticated: boolean;
+  hasRegistered: boolean;
   login: () => void;
   logout: () => void;
+  register: () => void;
 }
 
 export const useAuthStore = create<AuthState>(set => ({
   isAuthenticated: false,
+  hasRegistered: false,
   login: () => set({ isAuthenticated: true }),
   logout: () => set({ isAuthenticated: false }),
+  register: () => set({ hasRegistered: true }),
 }));
