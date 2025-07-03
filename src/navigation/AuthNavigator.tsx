589832a3f810303a47100b2dbec05cbd6095203b
abuabdulloh9599 <david@digitalnutrition.info>
2025-07-03 09:39:59 +0900

    feat: add ImageBackground to LoginScreen

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
 import RegisterScreen from '../screens/auth/RegisterScreen';
+import { StatusBar } from 'react-native';
 
 export type AuthStackParamList = {
   Login: undefined;
@@ -12,9 +13,17 @@ const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 export default function AuthNavigator() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
-    </Stack.Navigator>
+    <>
+      <StatusBar backgroundColor={'#000'} />
+
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </>
   );
 }
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -5,8 +5,10 @@ import {
   StyleSheet,
   TouchableOpacity,
   Platform,
+  ImageBackground,
 } from 'react-native';
 import { useAuthStore } from '../../store/authStore';
+
 import FaceBookIcon from '../../assets/icons/login/facebook.svg';
 import AppleIcon from '../../assets/icons/login/apple.svg';
 import GmailIcon from '../../assets/icons/login/google.svg';
@@ -18,34 +20,46 @@ export default function LoginScreen() {
   const login = useAuthStore(state => state.login);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>🔐 Login Screen</Text>
+    <ImageBackground
+      source={{
+        uri: 'https://img.freepik.com/free-vector/geometric-gradient-futuristic-background_23-2149116406.jpg?semt=ais_items_boosted&w=740',
+      }}
+      resizeMode="cover"
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>🔐 Login Screen</Text>
 
-      <View style={styles.socialContainer}>
-        {isIOS && (
+        <View style={styles.socialContainer}>
+          {isIOS && (
+            <TouchableOpacity style={styles.iconButton} onPress={login}>
+              <AppleIcon />
+            </TouchableOpacity>
+          )}
           <TouchableOpacity style={styles.iconButton} onPress={login}>
-            <AppleIcon />
+            <GmailIcon />
           </TouchableOpacity>
-        )}
-
-        <TouchableOpacity style={styles.iconButton} onPress={login}>
-          <GmailIcon />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.iconButton} onPress={login}>
-          <FaceBookIcon />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.iconButton} onPress={login}>
-          <NaverIcon />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.iconButton} onPress={login}>
-          <KakaoIcon />
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton} onPress={login}>
+            <FaceBookIcon />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton} onPress={login}>
+            <NaverIcon />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton} onPress={login}>
+            <KakaoIcon />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -54,6 +68,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     marginBottom: 20,
+    color: '#FFF',
   },
   socialContainer: {
     flexDirection: 'row',
